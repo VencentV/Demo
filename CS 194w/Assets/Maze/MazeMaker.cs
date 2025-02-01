8a8968d29dee6b574549fb6c173d3cb93a26e2cd
VencentV <104806343+VencentV@users.noreply.github.com>
2025-01-31 22:44:02 -0800

    Script error
    Unity is so annoying. Script that exist, doesn't exist. So, copy-paste into new script

--- CS 194w/Assets/Maze/MazeMaker.cs
+++ CS 194w/Assets/Maze/MazeMaker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class MazeGenerator : MonoBehaviour
+public class MakeMaker : MonoBehaviour
 {
 
     [SerializeField] private MazeCell mazeCellPrefab;
